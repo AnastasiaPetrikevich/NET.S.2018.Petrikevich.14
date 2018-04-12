5b3c639b0eb1d1a3927afec9dd5279c0574d7961
Петрикевич Анастасия <nastya.petrikevich@gmail.com>
2018-04-12 17:31:58 +0300

    Add project files.

---
+++ Algorithm/Algorithms.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public static class Algorithms
+    {
+        #region Binary search
+
+        /// <summary>
+        /// Binary search in a sorted array.
+        /// </summary>
+        /// <param name="array">Sorted ascending array.</param>
+        /// <param name="element">Search element.</param>
+        /// <param name="comparer">How to compare elements.</param>
+        /// <returns>Returns the index of searched element.</returns>
+        public static int BinarySearch<T>(T[] array, T element, Comparison<T> comparer)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} mustn't be null.");
+            }
+
+            if (comparer == null)
+            {
+                comparer = Default;
+            }
+
+            if (!IsArraySorted(array, comparer))
+            {
+                throw new ArgumentException($"{nameof(array)} must be sorted.");
+            }
+
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+
+            int first = 0;
+            int last = array.Length;
+
+            while (first < last)
+            {
+                int middle = first + (last - first) / 2;
+
+                if (comparer(element, array[middle]) == 0)
+                {
+                    return middle;
+                }
+
+                if (comparer(element, array[middle]) < 0)
+                {
+                    last = middle;
+                }
+
+                else
+                {
+                    first = middle + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Checks the array is sorted or not.
+        /// </summary>
+        /// <param name="array">Array.</param>
+        /// <param name="comparer">How to compare elements.</param>
+        /// <returns>Returns true if array sorted.</returns>
+        private static bool IsArraySorted<T>(T[] array, Comparison<T> comparer)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (comparer(array[i], array[i + 1]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Default comparison value.
+        /// </summary>
+        private static int Default<T>(T first, T second) =>
+            first.GetHashCode().CompareTo(second.GetHashCode());
+        
+        #endregion
+
+
+        #region Fibonacci numbers
+
+        /// <summary>
+        /// Generates a Fibonacci sequence of a given length.
+        /// </summary>
+        /// <param name="quantity">Quantity of a numbers in the sequance.</param>
+        /// <returns>Fibonacci sequance.</returns>
+        public static List<long> FibonacciSequance(int quantity)
+        {
+            if (quantity < 1)
+            {
+                throw new ArgumentNullException($"{nameof(quantity)} must be grater than or equal to 1");
+            }
+
+            List<long> numbers = new List<long>();
+            for (int i = 0; i < quantity; i++)
+            {
+                numbers.Add(FibonacciNumber(i));
+            }
+
+            return numbers;
+        }
+
+        /// <summary>
+        /// Find Fibonacci number.
+        /// </summary>
+        /// <param name="index">Index of a Fibonacci number.</param>
+        /// <returns>Fibonacci number.</returns>
+        private static long FibonacciNumber(int index)
+        {
+            long number = 0;
+
+            if (index > 1)
+            {
+                number = FibonacciNumber(index - 1) + FibonacciNumber(index - 2);
+            }
+
+            else
+            {
+                return 1;
+            }
+
+            return number;
+        }
+
+        #endregion
+    }
+}
---
+++ Queue.Logic/Queue.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue.Logic
+{
+    public sealed class Queue<T> 
+    {
+        #region Fields
+
+        private T[] values;
+        private int tail;
+        private int head;
+        private int capacity = 15;
+        private int count;
+
+        #endregion
+
+        #region Properties
+
+        public int Count => count;
+
+        #endregion
+        
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Queue()
+        {
+            values = new T[capacity];
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="capacity">Capacity of values array.</param>
+        public Queue(int capacity)
+        {
+            values = new T[capacity];
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="elements">List of elements.</param>
+        public Queue(IEnumerable<T> elements) : this()
+        {
+            if (elements == null)
+            {
+                throw new ArgumentNullException($"{nameof(elements)} mustn't be null.");
+            }
+
+            foreach (var i in elements)
+            {
+                Enqueue(i);
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Add element to queue.
+        /// </summary>
+        /// <param name="element">Element to be added.</param>
+        /// <exception cref="ArgumentNullException">Argument mustn't be null.</exception>
+        public void Enqueue(T element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException($"{nameof(element)} mustn't be null");
+            }
+
+            if (count >= capacity)
+            {
+                Resize();
+            }
+
+            values[tail] = element;
+            tail = count;
+            count++;
+        }
+
+        /// <summary>
+        /// Find and delete first element in queue.
+        /// </summary>
+        /// <returns>First element in queue.</returns>
+        public T Dequeue()
+        {
+            T element = values[head];
+            values[head] = default(T);
+            head += 1;
+            count--;
+            return element;
+        }
+
+        /// <summary>
+        /// Find first element in queue.
+        /// </summary>
+        /// <returns>First element in queue.</returns>
+        public T Peek() => values[head];
+
+        /// <summary>
+        /// Check if the queue contains the element.
+        /// </summary>
+        /// <param name="element">element to be checked</param>
+        /// <returns>True if queue contains the element.</returns>
+        public bool Contains(T element)
+        {
+            EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+            for (int i = 0; i < count; i++)
+            {
+                if (comparer.Equals(values[i], element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Iterator(this);
+        }
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Changes the size of values.
+        /// </summary>
+        private void Resize()
+        {
+            capacity *= 2;
+            T[] tempValues = new T[capacity];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                tempValues[i] = values[i];
+            }
+
+            values = tempValues;
+        }
+
+        #endregion
+
+        #region Iterator
+        private class Iterator : IEnumerator<T>
+        {
+            private Queue<T> queue = new Queue<T>();
+            private int current = -1;
+
+            public T Current => queue.values[current];
+
+            object IEnumerator.Current => Current;
+
+            public Iterator(Queue<T> queue)
+            {
+                this.queue = queue;
+            }
+
+            public bool MoveNext()
+            {
+                if (++current < queue.Count)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                current = -1;
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+
+    }
+
+}
